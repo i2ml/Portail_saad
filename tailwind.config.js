2139b4626fb135c0603d7ecab12da01861215ee0
Gabriel Amphoux <38751848+huiteuros@users.noreply.github.com>
2022-05-31 15:44:05 +0200

    #1
    Déploiement de tailwind et récupération des classes déjà existante
    Début du readme

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./app/**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "image-header": "url('/headerbg.png')"
+      },
+      colors: {
+        "blue-header-btn": "#006f78",
+        "yellow-header-title": "#f9b428",
+        "grey-recherche": "#f5f8f8",
+        "grey-footer": "#333333",
+      },
+    },
+  },
+  plugins: [],
+}
