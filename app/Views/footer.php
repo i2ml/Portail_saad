c69765918b557fcc93a8684aaf5dc5b9ee52b5ea
Gabriel Amphoux <38751848+huiteuros@users.noreply.github.com>
2022-06-01 09:44:01 +0200

    #3

---
+++ app/Views/footer.php
@@ -0,0 +1,6 @@
+<div class="text-center mt-20 p-6 bg-grey-footer">
+    <img class="w-1/12 inline" src="images/logo-gard.jpg">
+    <p class="text-white inline ml-20">Portail SAAD | <a class= "no-underline hover:underline text-blue-500" href="https://www.gard.fr/accueil.html"> Conseil départemental du Gard </a> - <a class= "no-underline hover:underline text-blue-500" href="https://www.i2ml.fr/"> Fondation i2ml </a> - 2022</p>
+</div>
+</body>
+</html>
--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Welcome to CodeIgniter 4!</title>
-	<meta name="description" content="The small framework with powerful features">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
-    <link rel = "StyleSheet" href = "style.css" type = "text / css" />
 
-</head>
-<body>
 
 
 <!-- CONTENT -->
- <p class="card"> TEST 2 </p>
+<p class="card"> TEST 2 </p>
 
 
-</body>
-</html>
