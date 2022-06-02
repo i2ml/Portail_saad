b3b68f6bbc3a457e618f6e0f1aa0ca3fbebfda02
BastLast <bastien.last87@gmail.com>
2022-06-02 16:17:43 +0200

    modele personne pour la page de connexion #10

---
+++ app/Models/PersonneModel.php
@@ -0,0 +1,15 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+/**
+ * PersonneModel
+ * @package App\Models
+ */
+class PersonneModel extends Model
+{
+    protected $table = 'personne';
+
+    protected $allowedFields = ['id', 'nom', 'prenom', 'mail', 'motdepasse', 'idSaad'];
+
+}
