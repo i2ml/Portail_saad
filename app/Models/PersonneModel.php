5d222c63f7e890b71afae9c6577facf14156a476
BastLast <bastien.last87@gmail.com>
2022-06-03 15:25:53 +0200

    Modification de la base de données pour les fonctionnalités d'administration. #30 #37 #33

--- app/Models/PersonneModel.php
+++ app/Models/PersonneModel.php
@@ -10,6 +10,6 @@ class PersonneModel extends Model
 {
     protected $table = 'personne';
 
-    protected $allowedFields = ['id', 'nom', 'prenom', 'mail', 'motdepasse', 'idSaad'];
+    protected $allowedFields = ['id', 'nom', 'prenom', 'mail', 'motdepasse', 'idSaadList', 'account_type'];
 
 }
