96c169ef449a94bd59e3f55976940e7d94d6a44c
Gabriel Amphoux <38751848+huiteuros@users.noreply.github.com>
2022-06-09 14:27:04 +0200

    #35
    Légère modification de l'interface de liste d'utilisateur

--- app/Views/userList.php
+++ app/Views/userList.php
@@ -2,16 +2,20 @@
 <?php if (!empty($users) && is_array($users)) { ?>
 
     <?php foreach ($users as $user) { ?>
-        <div class="card m-5">
+        <div class="card m-5 grid grid-cols-6">
             <p> <?php echo $user['nom'].' '.$user['prenom']; ?> </p>
             <?php if($user['accountType'] == 1) { ?>
                 <p> Admin </p>
-                <button class="blue-button"> Retrograder </button>
+                <form action="<?= esc(base_url()) ?>/AdminController/userDowngrade/<?= esc($user['id'], 'url'); ?>">
+                    <button class="blue-button"> Retrograder </button>
+                </form>
 
 
         <?php } else { ?>
                 <p> Gérant de Saad </p>
+                <form action="<?= esc(base_url()) ?>/AdminController/userUpgrade/<?= esc($user['id'], 'url'); ?>">
                     <button class="blue-button"> Passer admin </button>
+                </form>
                 <form action="<?= esc(base_url()) ?>/AdminController/userDelete/<?= esc($user['id'], 'url'); ?>" onclick="return confirm('Cette suppression est définitive, êtes vous certains de vouloir l\'effectuer ?')">
                     <button class="blue-button"> Supprimer </button>
                 </form>
