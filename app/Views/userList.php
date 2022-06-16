5794847a6fe943f73cdd4f250263cd489be7a08b
BastLast <bastien.last87@gmail.com>
2022-06-17 00:58:50 +0200

    fix #49

--- app/Views/userList.php
+++ app/Views/userList.php
@@ -3,23 +3,27 @@
 
     <?php foreach ($users as $user) { ?>
         <div class="card m-5 grid grid-cols-6">
-            <p> <?php echo $user['nom'].' '.$user['prenom']; ?> </p>
-            <?php if($user['accountType'] == 1) { ?>
-                <p> Admin </p>
-                <form action="<?= esc(base_url()) ?>/AdminController/userDowngrade/<?= esc($user['id'], 'url'); ?>">
-                    <button class="blue-button"> Retrograder </button>
-                </form>
-
-
-        <?php } else { ?>
-                <p> Gérant de Saad </p>
-                <form action="<?= esc(base_url()) ?>/AdminController/userUpgrade/<?= esc($user['id'], 'url'); ?>">
-                    <button class="blue-button"> Passer admin </button>
-                </form>
-                <form action="<?= esc(base_url()) ?>/AdminController/userDelete/<?= esc($user['id'], 'url'); ?>" onclick="return confirm('Cette suppression est définitive, êtes vous certains de vouloir l\'effectuer ?')">
-                    <button class="blue-button"> Supprimer </button>
-                </form>
-        <?php } ?>
+            <p> <?php echo $user['nom'] . ' ' . $user['prenom']; ?> </p>
+            <?php
+            if ($user['id'] === session()->get('id')) { ?>
+                <p> Votre compte </p>
+            <?php } else { ?>
+                <?php if ($user['accountType'] === SUPER_ADMIN) { ?>
+                    <p> Admin </p>
+                    <form action="<?= esc(base_url()) ?>/AdminController/userDowngrade/<?= esc($user['id'], 'url'); ?>">
+                        <button class="blue-button"> Rétrograder</button>
+                    </form>
+                <?php } else { ?>
+                    <p> Gérant de Saad </p>
+                    <form action="<?= esc(base_url()) ?>/AdminController/userUpgrade/<?= esc($user['id'], 'url'); ?>">
+                        <button class="blue-button"> Passer admin</button>
+                    </form>
+                    <form action="<?= esc(base_url()) ?>/AdminController/userDelete/<?= esc($user['id'], 'url'); ?>"
+                          onclick="return confirm('Cette suppression est définitive, êtes vous certains de vouloir l\'effectuer ?')">
+                        <button class="blue-button"> Supprimer</button>
+                    </form>
+                <?php }
+            } ?>
         </div>
 
     <?php } ?>
