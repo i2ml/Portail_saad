db0c8e5c2a43f4c430bc8d3f780cfb23f3669506
BastLast <bastien.last87@gmail.com>
2022-06-17 01:32:26 +0200

    Suppression d'une image inutile. + Ajout de sécurité sur la suppression de saad #50 + interface gérant de saad #32

--- app/Views/connexionReussie.php
+++ app/Views/connexionReussie.php
@@ -1,48 +1,52 @@
 <h2 class="title">
-    Espace administrateur
+    Espace <?php
+    if (session()->get('accountType') === SUPER_ADMIN) {
+        echo "administrateur";
+    } else {
+        echo "gestionnaire de saad";
+    } ?>
 </h2>
 <div class="container mx-auto">
     <?php
     //check if the user is a super admin
-    var_dump(session()->get('accountType'));
-    if(session()->get('accountType') === SUPER_ADMIN) {
-    ?>
+    if (session()->get('accountType') === SUPER_ADMIN) {
+        ?>
 
-    <h3 class="my-10 text-xl font-semibold">
-        <i class="fa-solid fa-user fa-xl"></i> Gestion des utilisateurs :
-    </h3>
-    <div class="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 xl:gap-7 md:gap-3 gap-1">
-        <a href="createUser">
-            <div class="menu-card">
-                <i class="fa-solid fa-plus fa-2xl text-blue-header-btn"></i>
-                <p class="mt-5">Créer un gérant de Saad</p>
-            </div>
-        </a>
-        <a href="userList">
-            <div class="menu-card">
-                <i class="fa-solid fa-trash fa-2xl text-blue-header-btn"></i>
-                <p class="mt-5">Supprimer un gérant de Saad</p>
-            </div>
-        </a>
-        <a href="userList">
-            <div class="menu-card">
-                <i class="fa-solid fa-medal fa-2xl text-blue-header-btn"></i>
-                <p class="mt-5">Promouvoir un gérant de Saad au rang d'administrateur</p>
-            </div>
-        </a>
-        <a href="userList">
-            <div class="menu-card">
-                <i class="fa-solid fa-arrow-trend-down fa-2xl text-blue-header-btn"></i>
-                <p class="mt-5">Rétrograder un administrateur au rang de gérant de Saad</p>
-            </div>
-        </a>
-        <a href="TODO">
-            <div class="menu-card">
-                <i class="fa-solid fa-link fa-2xl text-blue-header-btn"></i>
-                <p class="mt-5">Attribuer des SAAD</p>
-            </div>
-        </a>
-    </div>
+        <h3 class="my-10 text-xl font-semibold">
+            <i class="fa-solid fa-user fa-xl"></i> Gestion des utilisateurs :
+        </h3>
+        <div class="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 xl:gap-7 md:gap-3 gap-1">
+            <a href="createUser">
+                <div class="menu-card">
+                    <i class="fa-solid fa-plus fa-2xl text-blue-header-btn"></i>
+                    <p class="mt-5">Créer un gestionnaire de Saad</p>
+                </div>
+            </a>
+            <a href="userList">
+                <div class="menu-card">
+                    <i class="fa-solid fa-trash fa-2xl text-blue-header-btn"></i>
+                    <p class="mt-5">Supprimer un gestionnaire de Saad</p>
+                </div>
+            </a>
+            <a href="userList">
+                <div class="menu-card">
+                    <i class="fa-solid fa-medal fa-2xl text-blue-header-btn"></i>
+                    <p class="mt-5">Promouvoir un gestionnaire de Saad au rang d'administrateur</p>
+                </div>
+            </a>
+            <a href="userList">
+                <div class="menu-card">
+                    <i class="fa-solid fa-arrow-trend-down fa-2xl text-blue-header-btn"></i>
+                    <p class="mt-5">Rétrograder un administrateur au rang de gestionnaire de Saad</p>
+                </div>
+            </a>
+            <a href="TODO">
+                <div class="menu-card">
+                    <i class="fa-solid fa-link fa-2xl text-blue-header-btn"></i>
+                    <p class="mt-5">Attribuer des SAAD</p>
+                </div>
+            </a>
+        </div>
 
     <?php } ?>
 
@@ -50,24 +54,34 @@
         <i class="fa-solid fa-home fa-xl"></i> Gestions des saads :
     </h3>
     <div class="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 xl:gap-7 md:gap-3 gap-1">
-        <a href="createSaad">
-            <div class="menu-card">
-                <i class="fa-solid fa-plus fa-2xl text-blue-header-btn"></i>
-                <p class="mt-5">Créer un SAAD</p>
-            </div>
-        </a>
+        <?php
+        //check if the user is a super admin
+        if (session()->get('accountType') === SUPER_ADMIN) {
+            ?>
+            <a href="createSaad">
+                <div class="menu-card">
+                    <i class="fa-solid fa-plus fa-2xl text-blue-header-btn"></i>
+                    <p class="mt-5">Créer un SAAD</p>
+                </div>
+            </a>
+        <?php } ?>
         <a href="saadsList">
             <div class="menu-card">
                 <i class="fa-solid fa-pen fa-2xl text-blue-header-btn"></i>
                 <p class="mt-5">Modifier un SAAD</p>
             </div>
         </a>
-        <a href="saadsList">
-            <div class="menu-card">
-                <i class="fa-solid fa-trash fa-2xl text-blue-header-btn"></i>
-                <p class="mt-5">Supprimer un SAAD</p>
-            </div>
-        </a>
+        <?php
+        //check if the user is a super admin
+        if (session()->get('accountType') === SUPER_ADMIN) {
+            ?>
+            <a href="saadsList">
+                <div class="menu-card">
+                    <i class="fa-solid fa-trash fa-2xl text-blue-header-btn"></i>
+                    <p class="mt-5">Supprimer un SAAD</p>
+                </div>
+            </a>
+        <?php } ?>
     </div>
     <h3 class="my-10 text-xl font-semibold">
         <i class="fa-solid fa-gear fa-xl"></i> Paramètres :
--- app/Views/saadsList.php
+++ app/Views/saadsList.php
@@ -4,45 +4,48 @@
         <section class="all-saads">
 
             <?php foreach ($saads as $saad) { ?>
-                <article class="card border grid grid-cols-6 mt-5">
-                    <img
-                        class="col" src="<?php echo site_url('/images/logosaads/').$saad['image']; ?>">
-                    <div class="col">
-                        <h3 class="text-blue-header-btn text-2xl m-5">
-                            <?php echo $saad['nom'] ?>
-                        </h3>
+            <article class="card border grid grid-cols-6 mt-5">
+                <img
+                        class="col" src="<?php echo site_url('/images/logosaads/') . $saad['image']; ?>">
+                <div class="col">
+                    <h3 class="text-blue-header-btn text-2xl m-5">
+                        <?php echo $saad['nom'] ?>
+                    </h3>
 
-                        <?php if($saad['tel']) { ?>
+                    <?php if ($saad['tel']) { ?>
                         <p class="m-2">
                             Tel : <?php echo $saad['tel'] ?>
                         </p>
-                        <?php } ?>
-                        <?php if($saad['mail']) { ?>
+                    <?php } ?>
+                    <?php if ($saad['mail']) { ?>
                         <p class="m-2">
-                            E-mail : <a class= "link" href=""> <?php echo $saad['mail'] ?> </a>
+                            E-mail : <a class="link" href=""> <?php echo $saad['mail'] ?> </a>
                         </p>
-                        <?php } ?>
-                        <?php if($saad['site']) { ?>
+                    <?php } ?>
+                    <?php if ($saad['site']) { ?>
                         <p class="m-2">
-                            Site : <a class= "link" href=""> <?php echo $saad['site'] ?> </a>
+                            Site : <a class="link" href=""> <?php echo $saad['site'] ?> </a>
                         </p>
-                        <?php } ?>
-                    </div>
+                    <?php } ?>
+                </div>
+                <div class="col">
+                    <form action="<?= esc(base_url()) ?>/AdminController/createSaad/<?= esc($saad['id'], 'url'); ?>">
+                        <button class="blue-button"> Modifier</button>
+                    </form>
+                </div>
+                <?php
+                if (session()->get('accountType') === SUPER_ADMIN) {
+                    ?>
                     <div class="col">
-                        <form action="<?= esc(base_url()) ?>/AdminController/createSaad/<?= esc($saad['id'], 'url'); ?>" >
-                            <button class="blue-button"> Modifier </button>
+                        <form action="<?= esc(base_url()) ?>/AdminController/saadDelete/<?= esc($saad['id'], 'url'); ?>"
+                              onclick="return confirm('Cette suppression est définitive, êtes vous certains de vouloir l\'effectuer ?')">
+                            <button class="blue-button"> Supprimer</button>
                         </form>
                     </div>
-                    <div class="col">
-                        <form action="<?= esc(base_url()) ?>/AdminController/saadDelete/<?= esc($saad['id'], 'url'); ?>" onclick="return confirm('Cette suppression est définitive, êtes vous certains de vouloir l\'effectuer ?')">
-                            <button class="blue-button"> Supprimer </button>
-                        </form>
-                    </div>
-                </article>
-            </div>
-            <?php
-            }
-            ?>
-        </section>
+                <?php } ?>
+            </article>
+    </div>
+    <?php } ?>
+    </section>
     </div>
 </main>
