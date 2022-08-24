deb51890411247083da8b9d8fd7ff51d3b52e419
BastLast <bastien.last87@gmail.com>
2022-08-24 16:47:21 +0200

    gros refactor du saad controller

--- app/Controllers/SaadController.php
+++ app/Controllers/SaadController.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace App\Controllers;
 
 use App\Models\CiblerModel;
 use App\Models\PathologieModel;
-use App\Models\PublicModel;
 use App\Models\PersonneModel;
+use App\Models\PublicModel;
 use App\Models\SaadListModel;
 use App\Models\SaadModel;
 use App\Models\SpecialiserModel;
@@ -16,22 +17,37 @@ use CodeIgniter\HTTP\RedirectResponse;
  */
 class SaadController extends Controller
 {
+    private $saadModel;
+    private $saadListModel;
+    private $personneModel;
+    private $publicModel;
+    private $pathologieModel;
+    private $ciblerModel;
+    private $specialiserModel;
+
+    public function __construct()
+    {
+        $this->saadModel = new SaadModel();
+        $this->saadListModel = new SaadListModel();
+        $this->personneModel = new PersonneModel();
+        $this->publicModel = new PublicModel();
+        $this->pathologieModel = new PathologieModel();
+        $this->ciblerModel = new CiblerModel();
+        $this->specialiserModel = new SpecialiserModel();
+    }
 
     /**
      * Chargeuse de la page de recherche
      */
     public function index()
     {
-        $model = new SaadModel();
-        $modelPublic = new PublicModel();
-        $modelPathologie = new PathologieModel();
 
         $data = [
-            'saads' => $model->getSaads(),
-            'publics' => $modelPublic->getPublics(),
-            'pathologies' => $modelPathologie->getPathologies(),
+            'saads' =>  $this->saadModel->getSaads(),
+            'publics' =>  $this->publicModel->getPublics(),
+            'pathologies' =>  $this->pathologieModel->getPathologies(),
             'title' => 'Liste des Saads',
-            'idFiltrer' => $model->getAllSaadsId(),
+            'idFiltrer' =>  $this->saadModel->getAllSaadsId(),
         ];
 
         echo view('header', $data);
@@ -44,32 +60,24 @@ class SaadController extends Controller
      */
     public function filter()
     {
-        $model = new SaadModel();
-        $modelPublic = new PublicModel();
-        $modelPathologie = new PathologieModel();
-        $modelCibler = new CiblerModel();
-        $modelSpecialiser = new SpecialiserModel();
 
         $data = [
-            'saads' => $model->getSaads(),
-            'publics' => $modelPublic->getPublics(),
-            'pathologies' => $modelPathologie->getPathologies(),
+            'saads' => $this->saadModel->getSaads(),
+            'publics' => $this->publicModel->getPublics(),
+            'pathologies' => $this->pathologieModel->getPathologies(),
             'title' => 'Liste des Saads',
         ];
 
         $publicFilter = $this->request->getPost('public[]');
         $pathologieFilter = $this->request->getPost('pathologie[]');
+        $idSaadFiltrePathologie = $this->saadModel->getAllSaadsId();
+        $idSaadFiltrePublic = $this->saadModel->getAllSaadsId();
 
-        //Union tab : +
-        //Intersec tab :  array_intersect(array $array, array ...$arrays): array
-        $idSaadFiltrePathologie = $model->getAllSaadsId();
-        $idSaadFiltrePublic = $model->getAllSaadsId();
-
-        if(is_array($publicFilter)) {
-            $idSaadFiltrePublic = $modelCibler->getSaadsIdByIdPublic($publicFilter);
+        if (is_array($publicFilter)) {
+            $idSaadFiltrePublic = $this->ciblerModel->getSaadsIdByIdPublic($publicFilter);
         }
-        if(is_array($pathologieFilter)) {
-            $idSaadFiltrePathologie = $modelSpecialiser->getSaadsIdByIdPathologie($pathologieFilter);
+        if (is_array($pathologieFilter)) {
+            $idSaadFiltrePathologie = $this->specialiserModel->getSaadsIdByIdPathologie($pathologieFilter);
         }
 
         $data['pathologieSelectionnee'] = $pathologieFilter;
@@ -88,12 +96,9 @@ class SaadController extends Controller
      */
     public function saadsList()
     {
-        $model = new SaadModel();
-        $saadListModel = new SaadListModel();
-        $personneModel = new PersonneModel();
 
-        $saads = $model->getSaads();
-        $saads = $this->loadManagersInSaadListData($saads, $saadListModel, $personneModel);
+        $saads = $this->saadModel->getSaads();
+        $saads = $this->loadManagersInSaadListData($saads);
         $this->displaySaadList($saads, false);
     }
 
@@ -104,14 +109,9 @@ class SaadController extends Controller
      */
     public function mySaadsList($id)
     {
-        $model = new SaadModel();
-        $saadListModel = new SaadListModel();
-        $personneModel = new PersonneModel();
-
-        $saadsIds = $saadListModel->getSaadIdsFromPersonId($id);
-        $saads = $model->getSaadsByIds($saadsIds);
-
-        $saads = $this->loadManagersInSaadListData($saads, $saadListModel, $personneModel);
+        $saadsIds = $this->saadListModel->getSaadIdsFromPersonId($id);
+        $saads = $this->saadModel->getSaadsByIds($saadsIds);
+        $saads = $this->loadManagersInSaadListData($saads);
         $this->displaySaadList($saads, true);
     }
 
@@ -122,12 +122,8 @@ class SaadController extends Controller
      */
     public function saadDelete($id): RedirectResponse
     {
-
-        $model = new SaadModel();
-
-        $model->deleteImage($id);
-        $model->deleteLine($id);
-
+        $this->saadModel->deleteImage($id);
+        $this->saadModel->deleteLine($id);
 
         unset($data);
         return redirect()->to('saadsList');
@@ -142,23 +138,19 @@ class SaadController extends Controller
         $data = [];
         $session = session();
         $data['profil'] = $session->get('nom');
-        $publicModel = new PublicModel();
-        $data['publics'] = $publicModel->getPublics();
-        $pathologieModel = new PathologieModel();
-        $data['pathologies'] = $pathologieModel->getPathologies();
+        $data['publics'] = $this->publicModel->getPublics();
+        $data['pathologies'] = $this->pathologieModel->getPathologies();
         $data['title'] = 'Admin';
-        $model = new SaadModel();
         $data['success'] = null;
         $data['saad'] = $id;
         $data['publicsCible'] = [];
         $data['pathologiesSpecialise'] = [];
 
         if ($id) {
-            $ciblerModel = New CiblerModel();
-            $specialiserModel = New SpecialiserModel();
-            $data['saad'] = $model->getSaadbyid($id);
-            $data['publicsCible'] = $ciblerModel->getPublicsIdByIdSaad($id);
-            $data['pathologiesSpecialise'] = $specialiserModel->getPathologiesIdByIdSaad($id);
+
+            $data['saad'] = $this->saadModel->getSaadbyid($id);
+            $data['publicsCible'] = $this->ciblerModel->getPublicsIdByIdSaad($id);
+            $data['pathologiesSpecialise'] = $this->specialiserModel->getPathologiesIdByIdSaad($id);
         }
 
         echo view('header');
@@ -167,62 +159,32 @@ class SaadController extends Controller
     }
 
     /**
-     * Méthode appelée lorsque l'utilisateur a rentré les informations pour la creation d'un utilisateur
+     * Méthode appelée lorsque l'utilisateur a rentré les informations pour la creation d'un SAAD
      * @return RedirectResponse|void
      * @throws \ReflectionException
      */
     public function storeSaad($id = false)
     {
         helper(['form']);
-        $rules = [
-            'nom' => 'required|max_length[100]',
-            'tel' => 'max_length[100]|regex_match[/^((\+|00)33\s?|0)[1-9](\s?\d{2}){4}$/]',
-            'mail' => 'max_length[100]|valid_email',
-            'site' => 'max_length[150]',
-            'adresse' => "max_length[300]|regex_match[/^[a-zA-Z0-9\s,'-]*$/]",
-            'idCategorie' => 'required',
-            'image' => [
-                'rules' => 'is_image[image]'
-                    . '|mime_in[image,image/jpg,image/jpeg,image/gif,image/png,image/webp]'
-                    . '|max_size[image,100]'
-                    . '|max_dims[image,1024,768]',
-            ]
-        ];
-
-        $model = new SaadModel();
-        $ciblerModel = new CiblerModel();
-        $specialiserModel = new SpecialiserModel();
+        $rules = $this->getSaadFormRules();
+        //check if the rule are valid
         if ($this->validate($rules)) {
 
-            $data = [
-                'nom' => $this->request->getVar('nom'),
-                'tel' => $this->request->getVar('tel'),
-                'mail' => $this->request->getVar('mail'),
-                'site' => $this->request->getVar('site'),
-                'siret_siren' => $this->request->getVar('siret_siren'),
-                'adresse' => $this->request->getVar('adresse'),
-                'idCategorie' => $this->request->getVar('idCategorie'),
-            ];
-
+            $data = $this->createSaadFromFormInfo();
             $public = $this->request->getPost('public[]');
             $pathologie = $this->request->getPost('pathologie[]');
 
-
             if ($this->request->getFile('image')->getName() != "") {
-                $data = $data + ['image' => $this->request->getFile('image')->getName()];
-                $file = $this->request->getFile('image');
-                $file->store('../../public/images/logosaads', $file->getName());
+                $data = $this->saveImageFromFormFile($data);
             }
 
-            if ($id) {
-                $model->modifSaads($id, $data);
-                $specialiserModel->modifSpecialiser($pathologie, $id);
-                $ciblerModel->modifCibler($public, $id);
+            if ($id) { //check if the form is an update of an existing SAAD
+                $this->updateSaad($id, $data, $pathologie, $public);
                 $data['success'] = true;
             } else {
-                $id = $model->saveSaad($data);
-                $specialiserModel->saveAll($pathologie, $id);
-                $ciblerModel->saveAll($public, $id);
+                $id = $this->saadModel->saveSaad($data);
+                $this->specialiserModel->saveAll($pathologie, $id);
+                $this->ciblerModel->saveAll($public, $id);
             }
 
             return redirect()->to('/connexionReussie');
@@ -230,22 +192,18 @@ class SaadController extends Controller
 
         $session = session();
         $data['success'] = false;
-            if ($this->request->getMethod() !== 'post') {
-                $data['success'] = null;
-            }
+        if ($this->request->getMethod() !== 'post') {
+            $data['success'] = null;
+        }
         $data['profil'] = $session->get('nom');
         $data['validation'] = $this->validator;
         $data['title'] = 'Admin';
-        $publicModel = new PublicModel();
-        $data['publics'] = $publicModel->getPublics();
-        $pathologieModel = new PathologieModel();
-        $data['pathologies'] = $pathologieModel->getPathologies();
+        $data['publics'] = $this->publicModel->getPublics();
+        $data['pathologies'] = $this->pathologieModel->getPathologies();
         if ($id) {
-            $ciblerModel = New CiblerModel();
-            $specialiserModel = New SpecialiserModel();
-            $data['saad'] = $model->getSaadbyid($id);
-            $data['publicsCible'] = $ciblerModel->getPublicsIdByIdSaad($id);
-            $data['pathologiesSpecialise'] = $specialiserModel->getPathologiesIdByIdSaad($id);
+            $data['saad'] = $this->saadModel->getSaadbyid($id);
+            $data['publicsCible'] = $this->ciblerModel->getPublicsIdByIdSaad($id);
+            $data['pathologiesSpecialise'] = $this->specialiserModel->getPathologiesIdByIdSaad($id);
         } else {
             $data['saad'] = $id;
             $data['publicsCible'] = [];
@@ -275,20 +233,83 @@ class SaadController extends Controller
 
     /**
      * @param array $saads The list of saads that we need to search the manager
-     * @param SaadListModel $saadListModel The model that will search the saads
-     * @param PersonneModel $personneModel The model that will search the personnes
      * @return array the saads with all information about their managers
      */
-    private function loadManagersInSaadListData(array $saads, SaadListModel $saadListModel, PersonneModel $personneModel): array
+    private function loadManagersInSaadListData(array $saads): array
     {
         foreach ($saads as $key => $saad) {
             // on récupère la liste des personnes liées à ce saad sous forme de tableau d'id
-            $ids = $saadListModel->getPersonIdsFromSaadId($saad['id']);
+            $ids = $this->saadListModel->getPersonIdsFromSaadId($saad['id']);
             $saads[$key]['idsGerants'] = $ids;
             //on récupère les noms des personnes liées à ce saad pour les afficher plus facilement
-            $saads[$key]['noms'] = $personneModel->getPersonnesNameFromId($ids);
+            $saads[$key]['noms'] = $this->personneModel->getPersonnesNameFromId($ids);
         }
         return $saads;
+    }
+
+    /**
+     * @return array
+     */
+    private function getSaadFormRules(): array
+    {
+        return [
+            'nom' => 'required|max_length[100]',
+            'tel' => 'max_length[100]|regex_match[/^((\+|00)33\s?|0)[1-9](\s?\d{2}){4}$/]',
+            'mail' => 'max_length[100]|valid_email',
+            'site' => 'max_length[150]',
+            'adresse' => "max_length[300]|regex_match[/^[a-zA-Z0-9\s,'-]*$/]",
+            'idCategorie' => 'required',
+            'image' => [
+                'rules' => 'is_image[image]'
+                    . '|mime_in[image,image/jpg,image/jpeg,image/gif,image/png,image/webp]'
+                    . '|max_size[image,100]'
+                    . '|max_dims[image,1024,768]',
+            ],
+            'pathologie' => 'required',
+            'public' => 'required'
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    private function createSaadFromFormInfo(): array
+    {
+        return [
+            'nom' => $this->request->getVar('nom'),
+            'tel' => $this->request->getVar('tel'),
+            'mail' => $this->request->getVar('mail'),
+            'site' => $this->request->getVar('site'),
+            'siret_siren' => $this->request->getVar('siret_siren'),
+            'adresse' => $this->request->getVar('adresse'),
+            'idCategorie' => $this->request->getVar('idCategorie'),
+        ];
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    private function saveImageFromFormFile(array $data): array
+    {
+        $data = $data + ['image' => $this->request->getFile('image')->getName()];
+        $file = $this->request->getFile('image');
+        $file->store('../../public/images/logosaads', $file->getName());
+        return $data;
+    }
+
+    /**
+     * @param $id
+     * @param array $data
+     * @param $pathologie
+     * @param $public
+     * @throws \ReflectionException
+     */
+    private function updateSaad($id, array $data, $pathologie, $public): void
+    {
+        $this->saadModel->modifSaads($id, $data);
+        $this->specialiserModel->modifSpecialiser($pathologie, $id);
+        $this->ciblerModel->modifCibler($public, $id);
     }
 
 }
