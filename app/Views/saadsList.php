84081d22837b3b25d86685ac58abe025551f08a8
BastLast <bastien.last87@gmail.com>
2022-08-29 10:33:59 +0200

    suite de secteurlink #76

--- app/Views/saadsList.php
+++ app/Views/saadsList.php
@@ -65,6 +65,11 @@ if ($mySaadList) {
                                                 Modifier
                                             </button>
                                         </form>
+                                        <form action="<?= esc(base_url()) ?>/SaadController/linkSecteur/<?= esc($saad['id'], 'url'); ?>">
+                                            <button class="ml-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                                                Définir les secteurs d'action
+                                            </button>
+                                        </form>
                                         <?php
                                         // check if the user is a super admin
                                         if ($isAdmin) { ?>
