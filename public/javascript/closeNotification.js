2d96b27b705901b98f65f124aabcfc5dddf20b0b
BastLast <bastien.last87@gmail.com>
2022-08-30 15:45:02 +0200

    fix deepsource

--- public/javascript/closeNotification.js
+++ public/javascript/closeNotification.js
@@ -1,7 +1,11 @@
-let Notification = document.getElementById("notification");
+const Notification = document.getElementById("notification");
 Notification.style.transform = "translateX(150%)";
 Notification.classList.remove("hidden");
 Notification.style.transform = "translateX(0%)";
+
+/**
+ * @description Close the notification
+ */
 function closeModal() {
     Notification.style.transform = "translateX(150%)";
     Notification.classList.remove("hidden");
