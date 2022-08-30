eb3aa5131e7fa6fe68febadf2d2551b69dcd802d
BastLast <bastien.last87@gmail.com>
2022-08-30 11:12:15 +0200

    Ajout du filtrage par public sur la homepage #18

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,20 +2,29 @@
 
 namespace App\Controllers;
 
+use App\Models\PublicModel;
+
 /**
  * Base Controller
  * @package App\Controllers
  */
 class Home extends BaseController
 {
+    private $publicModel;
+
+    public function __construct()
+    {
+        $this->publicModel = new PublicModel();
+    }
 
     /**
-     * Chargeuse de la home page
+     * Chargeuse de la page d'accueil
      */
     public function index()
     {
+        $data['publics'] = $this->publicModel->getPublics();
         echo view('header');
-        echo view('welcome_message');
+        echo view('welcome_message' , $data);
         echo view('footer');
     }
 }
